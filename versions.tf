89a1b3160ea6e907190a6b72e3e551db92391df6
Xiong <wei.xiong@fujitsu.com>
2023-03-28 14:29:06 +1100

    Add organization

--- versions.tf
+++ versions.tf
@@ -13,7 +13,7 @@ terraform {
   required_version = ">= 0.15"
 
   backend "remote" {
-    organization = "<YOUR_TERRAFORM_ORG>"
+    organization = "Fujitsu-wx-training"
 
     workspaces {
       name = "sentinel-example"
